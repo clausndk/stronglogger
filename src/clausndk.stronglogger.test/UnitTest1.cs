190a8b7806674a32cdec3d83236373796b0e3915
Claus Nielsen <cln@sekoia.dk>
2024-11-05 19:24:31 +0100

    Empty logger and empty test project

---
+++ src/clausndk.stronglogger.test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace clausndk.stronglogger.test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/clausndk.stronglogger/IStrongLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace clausndk.stronglogger;
+
+public interface IStrongLogger
+{
+    void Log(LogLevel logLevel, string message);
+    void Log(LogLevel logLevel, Exception exception, string message);
+}
+
+public enum LogLevel
+{
+    Verbose,
+    Debug,
+    Information,
+    Warning,
+    Error,
+    Fatal
+}
